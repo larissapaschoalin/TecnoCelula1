e7b8cfa2afa734afef40ee8785a021a1cc56f476
Larissa <larissa.paschoalin@hotmail.com>
2017-08-31 12:11:08 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/usuario/tecnocelula1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.usuario.tecnocelula1;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView  bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.inicioItem) {
+                return false;
+                } else if (item.getItemId() == R.id.camaraItem) {
+
+                } else if (item.getItemId() == R.id.sobreItem) {
+                    startActivity(new Intent(MainActivity.this, SobreActivity.class));
+                }
+
+                return true;
+            }
+
+        });
+    }
+
+}
+
