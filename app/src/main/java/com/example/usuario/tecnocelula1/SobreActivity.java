6447e5e8a5db40c07febdad6e4d930fce51b5274
Larissa <larissa.paschoalin@hotmail.com>
2017-09-01 14:38:18 -0300

    primeiro commit

--- app/src/main/java/com/example/usuario/tecnocelula1/SobreActivity.java
+++ app/src/main/java/com/example/usuario/tecnocelula1/SobreActivity.java
@@ -1,13 +1,36 @@
 package com.example.usuario.tecnocelula1;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SobreActivity extends AppCompatActivity {
-
+    private BottomNavigationView  bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.inicioItem) {
+                    startActivity(new Intent(SobreActivity.this, MainActivity.class));
+
+                } else if (item.getItemId() == R.id.camaraItem) {
+
+                } else if (item.getItemId() == R.id.sobreItem) {
+                    return false;
+                }
+
+                return true;
+            }
+
+        });
+
     }
 }
