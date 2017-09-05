97573171747a61bc1c7e16e630975833e0706d0e
Larissa <larissa.paschoalin@hotmail.com>
2017-09-05 11:07:01 -0300

    Tela complexo funcionando. Botao cinza

--- app/src/main/java/com/example/usuario/tecnocelula1/ComplexoActivity.java
+++ app/src/main/java/com/example/usuario/tecnocelula1/ComplexoActivity.java
@@ -1,13 +1,46 @@
 package com.example.usuario.tecnocelula1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ComplexoActivity extends AppCompatActivity {
-
+    private ImageButton button_inicio;
+    private ImageButton buttonCamera;
+    private ImageButton buttonInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_complexo);
+        setContentView(R.layout.activity_inicial);
+
+        button_inicio = (ImageButton) findViewById(R.id.button_inicio);
+        button_inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ComplexoActivity.this, InicialActivity.class));
+            }
+        });
+        buttonCamera = (ImageButton) findViewById(R.id.buttonCamera);
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ir para tela
+            }
+
+        });
+
+        buttonInfo = (ImageButton) findViewById(R.id.lbuttonInfo);
+
+        buttonInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ComplexoActivity.this, SobreActivity.class));
+            }
+        });
+
+
     }
 }
